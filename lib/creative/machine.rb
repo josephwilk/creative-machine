f61c0cc95696c535b2653d4d3cca51bb871093c0
Joseph Wilk <joe@josephwilk.net>
2012-07-25 14:39:58 +0100

    Begin working on the poet

---
+++ lib/creative/machine.rb
@@ -0,0 +1,2 @@
+module Machine
+end
---
+++ lib/creative/machine/poet.rb
@@ -0,0 +1,16 @@
+#The Creative poet: Based on www.ncbi.nlm.nih.gov/pubmed/18677746
+
+module Machine
+  class Generator
+  end
+  class Evaluator
+  end
+  class Workspace
+  end
+  class Lexicon
+  end
+  class ConceptualKnowledgeBase
+  end
+  class SyntacticalKnowledgeBase
+  end
+end
