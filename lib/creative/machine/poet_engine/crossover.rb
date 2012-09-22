5c4667a51b90ba920b21ddd1ae57793e90470830
josephwilk <joe@josephwilk.net>
2012-09-22 15:44:59 -0600

    Simple cross over experiements

--- lib/creative/machine/poet_engine/crossover.rb
+++ lib/creative/machine/poet_engine/crossover.rb
@@ -5,12 +5,21 @@ module Creative
       class Crossover
         
         class << self
-          def crossover(poem1, poem2)
+          def crossover(poem_1, poem_2)
             #first it may merge the former poem's minilex with the latter's or ignore the latter minilex or do nothing
             #it may take into consideration the other poem's rhyming or ignore the other poem's rhyming altogether or do nothing
             #it will either work with the former and the latter's lines or ignore the latter's lines or do nothing
             
-            poem1
+            poem = [poem_1, poem_2][rand(1)]
+            poem = crossover_lines(poem_1, poem_2) if crossover?
+            poem
+          end
+          
+          def crossover_lines(poem_1, poem_2)
+            new_poem = poem_1
+            new_poem[0] = poem_2[0]
+            new_poem[-1] = poem_2[-1]
+            new_poem
           end
 
           private
