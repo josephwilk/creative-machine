727353b1082e3d48708d08a93f564ef6270dd667
josephwilk <joe@josephwilk.net>
2012-09-25 10:25:47 -0700

    Refactoring: Cleaning up code to be more readable

--- lib/creative/machine/poet_engine/crossover.rb
+++ lib/creative/machine/poet_engine/crossover.rb
@@ -6,10 +6,6 @@ module Creative
         
         class << self
           def crossover(poem_1, poem_2)
-            #first it may merge the former poem's minilex with the latter's or ignore the latter minilex or do nothing
-            #it may take into consideration the other poem's rhyming or ignore the other poem's rhyming altogether or do nothing
-            #it will either work with the former and the latter's lines or ignore the latter's lines or do nothing
-            
             poem = [poem_1, poem_2][rand(1)]
             poem = crossover_lines(poem_1, poem_2) if crossover?
             poem
@@ -19,12 +15,8 @@ module Creative
             child_1 = poem_1
             child_2 = poem_2
             
-            
-            child_1[0] = poem_2[0]
-            child[-1] = poem_2[-1]
-            
-            child_2[0] = poem_1[0]
-            child_2[-1] = poem_1[-1]
+            child_1[0],  child_2[0] =  poem_2[0], poem_1[0]
+            child_1[-1], child_2[-1] = poem_2[-1], poem_1[-1]
             
             [child_1, child_2]
           end
