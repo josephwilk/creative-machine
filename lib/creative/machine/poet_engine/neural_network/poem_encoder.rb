f09fd1af0225cd8ae12a790cf6a7e00c9ede91c2
josephwilk <joe@josephwilk.net>
2012-10-02 18:24:26 +0100

    Ensure we correctly encode a 66 bit binary number for the syllable. Clean up some of the padding so we only process array of 0/1s rather than strings

--- lib/creative/machine/poet_engine/neural_network/poem_encoder.rb
+++ lib/creative/machine/poet_engine/neural_network/poem_encoder.rb
@@ -23,19 +23,17 @@ module Creative
           def encode_word(word)
             lexicon_index_in_binary = binary_lexicon_word_code(word)
             Lexicon.syllables_in(word).each_with_index.map {|syllable, syllable_index| 
-               encode_syllable(word, syllable, syllable_index)
-            }.
-            map{|syllable| lexicon_index_in_binary + syllable}
+              lexicon_index_in_binary + encode_syllable(word, syllable, syllable_index)
+            }
           end
           
           def encode_syllable(word, syllable, syllable_index)
             syllable_count = Lexicon.syllables_in(word).count
              
             phonemes = Lexicon.phonemes_for(word)
-            if syllable_count == 1  
-              code = encode_phonemes(phonemes[0]).flatten.join
-              code = pad(code, BITS_FOR_SYLLABLES)
-              code.split('').map(&:to_i)
+            if syllable_count == 1
+              code = encode_phonemes(phonemes[0]).flatten
+              pad(code, BITS_FOR_SYLLABLES)
             else
               [0] * BITS_FOR_SYLLABLES
             end
@@ -59,13 +57,13 @@ module Creative
           def binary_lexicon_word_code(word)
             index = @lexicon.index(word)
             raise Exception.new("Cannot find word in lexicon: #{word}") unless index
-            index_binary = pad(index.to_s(2), BITS_FOR_LEXICON_INDEX)
-            index_binary.split('').map(&:to_i)
+            index_binary = index.to_s(2).split('').map(&:to_i)
+            pad(index_binary, BITS_FOR_LEXICON_INDEX)
           end
           
           def pad(input, size)
             pad_length = size - input.length
-            "0" * pad_length + input
+            pad_length > 0 ? ([0] * pad_length) + input : input
           end
 
         end
