e2e50dd8b17fdebf56e13a160d5035a32670eb66
josephwilk <joe@josephwilk.net>
2012-11-05 09:21:33 -0700

    Setup fixtures with training data for tests

--- spec_integration/spec_helper.rb
+++ spec_integration/spec_helper.rb
@@ -1,3 +1,10 @@
 $:.unshift(File.dirname(__FILE__) + '/lib') unless $:.include?(File.dirname(__FILE__) + '/lib')
 
 require 'creative_machine'
+
+RSpec.configure do |c|
+  c.before(:all) do
+    root = File.dirname(__FILE__) + '/../'
+    FileUtils.cp("#{root}/spec_integration/fixtures/*", "#{root}/data/weights/")
+  end
+end
