22a1b99c59152a0810749cfb827c9547a0b3d293
josephwilk <joe@josephwilk.net>
2012-11-24 18:18:30 +0100

    We were not processing all the haikus when training

--- tasks/learn.rb
+++ tasks/learn.rb
@@ -1,11 +1,12 @@
 namespace :learn do
   haikus_rankings = [
-    ["on the cherry glass",  "even worn lost rain of all",   "and it looks not the"] => 1,
-    ["after all sign far",   "see tide under just end am",   "the rice many am"]     => 1,
-    ["being cherry the",     "amid hair rain harvest the",   "of shelter to why"]    => 1,
-    ["of white inscription", "have the to the youth coming", "not very her at"]      => 1,
-    ["ugly bend radish",     "warriors is our autumn",       "take needles shoots the"] => 1,
-    ["dripping morning snow","rain violets are striking",    "autumn bites day fresh"] => 3
+    {["on the cherry glass",  "even worn lost rain of all",      "and it looks not the"]    => 1},
+    {["after all sign far",   "see tide under just end am",      "the rice many am"]        => 1},
+    {["being cherry the",     "amid hair rain harvest the",      "of shelter to why"]       => 1},
+    {["of white inscription", "have the to the youth coming",    "not very her at"]         => 1},
+    {["ugly bend radish",     "warriors is our autumn",          "take needles shoots the"] => 1},
+    {["dripping morning snow","rain violets are striking",       "autumn bites day fresh"]  => 3},
+    {["misty chestnut moon",  "wild embrace that changes night", "like heat on the snow"]   => 6}
   ]
   
   def tokenise_haiku(sentences)
