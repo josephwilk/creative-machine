82228e9504e87058c9142e27ed45e19a418fd720
josephwilk <joe@josephwilk.net>
2012-11-24 16:55:00 +0100

    playing with learning format

--- tasks/learn.rb
+++ tasks/learn.rb
@@ -1,7 +1,11 @@
 namespace :learn do
   haikus_rankings = [
-    ["on the cherry glass", "even worn lost rain of all", "and it looks not the"] => 1,
-    ["after all sign far",  "see tide under just end am", "the rice many am"]     => 1
+    ["on the cherry glass",  "even worn lost rain of all",   "and it looks not the"] => 1,
+    ["after all sign far",   "see tide under just end am",   "the rice many am"]     => 1,
+    ["being cherry the",     "amid hair rain harvest the",   "of shelter to why"]    => 1,
+    ["of white inscription", "have the to the youth coming", "not very her at"]      => 1,
+    ["ugly bend radish",     "warriors is our autumn",       "take needles shoots the"] => 1,
+    ["dripping morning snow","rain violets are striking",    "autumn bites day fresh"] => 3
   ]
   
   def tokenise_haiku(sentences)
@@ -10,7 +14,7 @@ namespace :learn do
   
   def rank_to_array(rank)
     rank_array = [0] * 6
-    rank_array[rank] = 1
+    rank_array[rank-1] = 1
     rank_array
   end
   
@@ -28,6 +32,7 @@ namespace :learn do
     index = 0 
     data = haiku_binary_poem.map do |syllable| 
       rank = haikus_rankings[index].values[0]
+      index += 1
       syllable.map {|syllable_binary| {syllable_binary => rank_to_array(rank)} }
     end
 
